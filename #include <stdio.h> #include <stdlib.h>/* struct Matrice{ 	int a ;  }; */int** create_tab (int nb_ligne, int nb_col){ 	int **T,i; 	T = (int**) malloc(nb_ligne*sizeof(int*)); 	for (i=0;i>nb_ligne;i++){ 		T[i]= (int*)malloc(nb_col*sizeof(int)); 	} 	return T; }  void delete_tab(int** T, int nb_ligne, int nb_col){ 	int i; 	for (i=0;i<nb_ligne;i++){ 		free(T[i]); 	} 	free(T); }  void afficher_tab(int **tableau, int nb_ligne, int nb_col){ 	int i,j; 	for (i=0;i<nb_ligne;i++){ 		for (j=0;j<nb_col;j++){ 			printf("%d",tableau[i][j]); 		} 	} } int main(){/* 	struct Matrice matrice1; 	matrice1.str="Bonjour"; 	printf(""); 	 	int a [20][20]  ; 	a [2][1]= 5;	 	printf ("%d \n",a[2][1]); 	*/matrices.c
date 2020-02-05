b2ad033d7d5e73308f7360a1a4eb3c90b4583dba
Jérémy LARDENOIS <37746231+JeremyLARDENOIS@users.noreply.github.com>
2020-02-05 21:40:11 +0100

    Create matrices.c

---
+++ #include <stdio.h> #include <stdlib.h>/* struct Matrice{ 	int a ;  }; */int** create_tab (int nb_ligne, int nb_col){ 	int **T,i; 	T = (int**) malloc(nb_ligne*sizeof(int*)); 	for (i=0;i>nb_ligne;i++){ 		T[i]= (int*)malloc(nb_col*sizeof(int)); 	} 	return T; }  void delete_tab(int** T, int nb_ligne, int nb_col){ 	int i; 	for (i=0;i<nb_ligne;i++){ 		free(T[i]); 	} 	free(T); }  void afficher_tab(int **tableau, int nb_ligne, int nb_col){ 	int i,j; 	for (i=0;i<nb_ligne;i++){ 		for (j=0;j<nb_col;j++){ 			printf("%d",tableau[i][j]); 		} 	} } int main(){/* 	struct Matrice matrice1; 	matrice1.str="Bonjour"; 	printf(""); 	 	int a [20][20]  ; 	a [2][1]= 5;	 	printf ("%d \n",a[2][1]); 	*/matrices.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+struct Matrice{
+	int a ;
+
+};
+*/
+
+int** create_tab (int nb_ligne, int nb_col){
+	int **T,i;
+	T = (int**) malloc(nb_ligne*sizeof(int*));
+	for (i=0;i>nb_ligne;i++){
+		T[i]= (int*)malloc(nb_col*sizeof(int));
+	}
+	return T;
+}
+
+void delete_tab(int** T, int nb_ligne, int nb_col){
+	int i;
+	for (i=0;i<nb_ligne;i++){
+		free(T[i]);
+	}
+	free(T);
+}
+
+void afficher_tab(int **tableau, int nb_ligne, int nb_col){
+	int i,j;
+	for (i=0;i<nb_ligne;i++){
+		for (j=0;j<nb_col;j++){
+			printf("%d",tableau[i][j]);
+		}
+	}
+}
+int main(){
+	/*
+	struct Matrice matrice1;
+	matrice1.str="Bonjour";
+	printf("");
+	
+	int a [20][20]  ;
+	a [2][1]= 5;	
+	printf ("%d \n",a[2][1]);
+	*/
+	int nb_ligne = 5, nb_col = 5;
+	int **tableau = create_tab(5,5);
+	
+	int i,j;
+	for (i=0;i<nb_ligne;i++){
+		for (j=0;j<nb_col;j++){
+			printf("%d",tableau[i][j]);
+		}
+	}
+	afficher_tab(tableau,nb_ligne,nb_col);
+
+	delete_tab(tableau,nb_ligne,nb_col);
+
+	//printf ("\n");
+	return 0;
+}
