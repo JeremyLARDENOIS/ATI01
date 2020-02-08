99bac394dfa8fab037eb5eabc4642d51573e07dd
JeremyLARDENOIS <jeremy.lardenois@hotmail.fr>
2020-02-08 16:57:06 +0000

    debut de créeation d'une fonction créatrice de matrice

--- matrices.c
+++ matrices.c
@@ -7,7 +7,7 @@ typedef struct Matrice{
     	int nb_ligne;
 	int nb_col;
 } Matrice;
-
+/*--------------------------------------------------------*/
 double** create_tab (int nb_ligne, int nb_col ){
 	  double **T;
 	  int i;
@@ -18,13 +18,34 @@ double** create_tab (int nb_ligne, int nb_col ){
 		  return T;
 }
 
-void delete_tab(Matrice *m){
+void delete_tab (Matrice *m){
 	  int i;
 	  for (i=0;i<m->nb_ligne;i++){
 		free(m->matrice[i]);
 			  }
 	      free(m->matrice);
 }
+
+Matrice create_matrice(){
+	Matrice m;
+	printf("Donnez un nom a la matrice: ");
+	scanf("%s",&m.nom);
+	printf("Quel est la dimension de la matrice %s (<nb_ligne>x<nb_col>) ?: ",m.nom);
+	scanf("%dx%d",&m.nb_ligne,&m.nb_col);
+	m.matrice = create_tab(m.nb_ligne,m.nb_col);
+	printf("Remplissons la matrice \"%s\" de taille %dx%d: \n",m.nom,m.nb_ligne,m.nb_col);
+	int i,j;
+	
+	for (i=0;i<m.nb_ligne;i++){
+	        for (j=0;j<m.nb_col;j++){
+			printf("%s [%d] [%d]",m.nom,i,j);      
+			scanf("%lf",&m.matrice[i][j]);
+		}
+	
+	  }
+	  return m ;
+}
+
 
 void afficher_tab(Matrice *m){
 	  int i,j;
@@ -36,20 +57,12 @@ void afficher_tab(Matrice *m){
 			    printf("\n");
 			      }
 }
+/*---------------------------------------------------------------------------*/
 int main(){
-	Matrice m1 = {"matrice n°1",create_tab(5,5),5,5};
-		    /*//Decommenter pour avoir une matrice non-nul
-		     *   int i,j;
-		     *     for (i=0;i<nb_ligne;i++){
-		     *         for (j=0;j<nb_col;j++){
-		     *               tableau[i][j]=(double)i+j;
-		     *                   }
-		     *                       printf("\n");
-		     *                         }
-		     *                           */
+	Matrice m1;
+	m1 = create_matrice();
 	afficher_tab(&m1);
-
 	delete_tab(&m1);
 
-		        return 0;
+	return 0;
 }
