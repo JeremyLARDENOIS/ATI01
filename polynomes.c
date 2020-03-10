871da5cdb24a48cf28f491a8dc0106d71f393382
jerem <jeremy.lardenois@hotmail.fr>
2020-03-10 23:13:54 +0100

    debut poly

---
+++ polynomes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*------------Structure-----------------*/
+
+Struct S_Polynome {
+	double* tab;
+	int degre;
+};
+
+
+
+/*-------------Fonction-----------------*/
+
+
+/*---------------Main-------------------*/
+
+int main (){
+
+	return 0;
+}
